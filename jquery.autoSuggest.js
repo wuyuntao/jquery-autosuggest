7e87c5932cdfc20dae6c0d16009d2281b2a4401c
vlad <iamvlad@gmail.com>
2011-11-03 16:35:10 -0700

    fixed typo in header comment

--- jquery.autoSuggest.js
+++ jquery.autoSuggest.js
@@ -14,7 +14,7 @@
  * the fly. It supports keybord navigation (UP + DOWN + RETURN), as well
  * as multiple AutoSuggest fields on the same page.
  *
- * Inspied by the Autocomplete plugin by: J歳n Zaefferer
+ * Inspired by the Autocomplete plugin by: J歳n Zaefferer
  * and the Facelist plugin by: Ian Tearle (iantearle.com)
  *
  * This AutoSuggest jQuery plug-in is dual licensed under the MIT and GPL licenses:
